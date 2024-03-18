c6cfad30b12c2afa09da4955564584c1c55a3490
Gabriel Pretel <gabrilinex@gmail.com>
2024-03-18 17:07:23 +0100

    Inicio del ejericio

---
+++ src/main.spec.ts
@@ -0,0 +1,166 @@
+import {
+  LineaTicket,
+  calculaLineaTicket,
+  ResultadoLineaTicket,
+  calcularPrecioConIva,
+  TipoIva,
+} from "./main";
+
+describe("calculaLineaTicket", () => {
+  it("Deberia devolver una linea del ticket", () => {
+    // Arrange
+    const productos: LineaTicket[] = [
+      {
+        producto: {
+          nombre: "Legumbres",
+          precio: 2,
+          tipoIva: "general",
+        },
+        cantidad: 2,
+      },
+      {
+        producto: {
+          nombre: "Perfume",
+          precio: 20,
+          tipoIva: "general",
+        },
+        cantidad: 3,
+      },
+    ];
+
+    const resultadoEsperado: ResultadoLineaTicket[] = [
+      {
+        nombre: "Legumbres",
+        cantidad: 2,
+        precioSinIva: 4,
+        precioConIva: 4.84,
+        tipoIva: "general",
+      },
+      {
+        nombre: "Perfume",
+        cantidad: 3,
+        precioSinIva: 60,
+        precioConIva: 72.6,
+        tipoIva: "general",
+      },
+    ];
+
+    // Act
+    const resultado = calculaLineaTicket(productos);
+
+    //Assert
+    expect(resultado).toStrictEqual(resultadoEsperado);
+  });
+
+  it("Deberia devolver una linea del ticket", () => {
+    // Arrange
+    const productos: LineaTicket[] = [
+      {
+        producto: {
+          nombre: "Leche",
+          precio: 1,
+          tipoIva: "superreducidoC",
+        },
+        cantidad: 4,
+      },
+      {
+        producto: {
+          nombre: "Lasaña",
+          precio: 5,
+          tipoIva: "superreducidoA",
+        },
+        cantidad: 3,
+      },
+    ];
+
+    const resultadoEsperado: ResultadoLineaTicket[] = [
+      {
+        nombre: "Leche",
+        cantidad: 4,
+        precioSinIva: 4,
+        precioConIva: 4,
+        tipoIva: "superreducidoC",
+      },
+      {
+        nombre: "Lasaña",
+        cantidad: 3,
+        precioSinIva: 15,
+        precioConIva: 15.75,
+        tipoIva: "superreducidoA",
+      },
+    ];
+
+    // Act
+    const resultado = calculaLineaTicket(productos);
+
+    //Assert
+    expect(resultado).toStrictEqual(resultadoEsperado);
+  });
+});
+
+describe("calcularPrecioConIva", () => {
+  it("Debería devolver el precio total del producto con IVA general", () => {
+    // Arrange
+    const precio = 4;
+    const tipoIva: TipoIva = "general";
+    const resultadoEsperado: number = 4.84;
+
+    // Act
+    const resultado = calcularPrecioConIva(precio, tipoIva);
+
+    //Assert
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+  it("Debería devolver el precio total del producto con IVA reducido", () => {
+    // Arrange
+    const precio = 7;
+    const tipoIva: TipoIva = "reducido";
+    const resultadoEsperado: number = 7.7;
+
+    // Act
+    const resultado = calcularPrecioConIva(precio, tipoIva);
+
+    //Assert
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+  it("Debería devolver el precio total del producto con IVA superreducido A", () => {
+    // Arrange
+    const precio = 15;
+    const tipoIva: TipoIva = "superreducidoA";
+    const resultadoEsperado: number = 15.75;
+
+    // Act
+    const resultado = calcularPrecioConIva(precio, tipoIva);
+
+    //Assert
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+  it("Debería devolver el precio total del producto con IVA superreducido B", () => {
+    // Arrange
+    const precio = 17;
+    const tipoIva: TipoIva = "superreducidoB";
+    const resultadoEsperado: number = 17.68;
+
+    // Act
+    const resultado = calcularPrecioConIva(precio, tipoIva);
+
+    //Assert
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+  it("Debería devolver el precio total del producto con IVA superreducido C", () => {
+    // Arrange
+    const precio = 1.5;
+    const tipoIva: TipoIva = "superreducidoC";
+    const resultadoEsperado: number = 1.5;
+
+    // Act
+    const resultado = calcularPrecioConIva(precio, tipoIva);
+
+    //Assert
+    expect(resultado).toBe(resultadoEsperado);
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,146 @@
-import "./style.css";
+const productos: LineaTicket[] = [
+  {
+    producto: {
+      nombre: "Legumbres",
+      precio: 2,
+      tipoIva: "general",
+    },
+    cantidad: 2,
+  },
+  {
+    producto: {
+      nombre: "Perfume",
+      precio: 20,
+      tipoIva: "general",
+    },
+    cantidad: 3,
+  },
+  {
+    producto: {
+      nombre: "Leche",
+      precio: 1,
+      tipoIva: "superreducidoC",
+    },
+    cantidad: 6,
+  },
+  {
+    producto: {
+      nombre: "Lasaña",
+      precio: 5,
+      tipoIva: "superreducidoA",
+    },
+    cantidad: 1,
+  },
+];
 
-console.log("Hello Typescript!");
+export type TipoIva =
+  | "general"
+  | "reducido"
+  | "superreducidoA"
+  | "superreducidoB"
+  | "superreducidoC"
+  | "sinIva";
+
+export interface Producto {
+  nombre: string;
+  precio: number;
+  tipoIva: TipoIva;
+}
+
+export interface LineaTicket {
+  producto: Producto;
+  cantidad: number;
+}
+
+export interface ResultadoLineaTicket {
+  nombre: string;
+  cantidad: number;
+  precioSinIva: number;
+  tipoIva: TipoIva;
+  precioConIva: number;
+}
+
+interface ResultadoTotalTicket {
+  totalSinIva: number;
+  totalConIva: number;
+  totalIva: number;
+}
+
+interface TotalPorTipoIva {
+  tipoIva: TipoIva;
+  cuantia: number;
+}
+
+interface TicketFinal {
+  lineas: ResultadoLineaTicket[];
+  total: ResultadoTotalTicket;
+  desgloseIva: TotalPorTipoIva[];
+}
+
+// const calculaTicket = (lineasTicket: LineaTicket[]) => {
+//   // 1.Calculamos la cantidad de un producto
+//   lineasTicket.reduce((acc: number, linea: LineaTicket) => {});
+// };
+
+// calculamos todas las líneas del ticket
+
+export const calculaLineaTicket = (
+  lineas: LineaTicket[]
+): ResultadoLineaTicket[] => {
+  const resultadoLineasTicket = lineas.map((linea) => {
+    const cantidad = linea.cantidad;
+    const { nombre, precio, tipoIva } = linea.producto;
+    const precioSinIva = precio * cantidad;
+    const precioConIva = calcularPrecioConIva(precioSinIva, tipoIva);
+
+    return {
+      nombre: nombre,
+      cantidad: cantidad,
+      precioSinIva: precioSinIva,
+      tipoIva: tipoIva,
+      precioConIva: precioConIva,
+    };
+  });
+  return resultadoLineasTicket;
+};
+
+export const calcularPrecioConIva = (precio: number, iva: TipoIva): number => {
+  switch (iva) {
+    case "general":
+      return precio * 0.21 + precio;
+    case "reducido":
+      return precio * 0.1 + precio;
+    case "superreducidoA":
+      return precio * 0.05 + precio;
+    case "superreducidoB":
+      return precio * 0.04 + precio;
+    case "superreducidoC":
+      return precio;
+    case "sinIva":
+      return precio;
+  }
+};
+
+console.log(calculaLineaTicket(productos));
+
+const calculaTotalIva = (lineas: ResultadoLineaTicket[]): TotalPorTipoIva[] => {
+  return lineas.reduce((acumulador, linea) => {
+    // Encuentra el índice del tipo de IVA actual en el acumulador
+    const indice = acumulador.findIndex(
+      (item) => item.tipoIva === linea.tipoIva
+    );
+    if (indice === -1) {
+      // Si no existe, lo añade al acumulador
+      acumulador.push({
+        tipoIva: linea.tipoIva,
+        cuantia: linea.precioConIva,
+      });
+    } else {
+      // Si ya existe, suma la cuantía
+      acumulador[indice].cuantia += linea.precioConIva;
+    }
+    return acumulador;
+  }, [] as TotalPorTipoIva[]);
+};
+
+console.log(calculaTotalIva(calculaLineaTicket(productos)));
